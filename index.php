9a40cc5176c5ec35c155db7eace931c6588444cb
AugustoJmnz <90275963+AugustoJmnz@users.noreply.github.com>
2023-10-16 20:22:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Hello World, Azure!";
+    $dbhost = "aplicacionwebbasededato-server.mysql.database.azure.com";
+    $dbuser = "sxtsaygsbk";
+    $dbpassword = "IC2540W4VMB01YW1$";
+    $dbname = "aplicacionwebbasededatos-database";
+    $port = 3306;
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $port);
+    if (!$conn){
+        die("No hay conexión: ".mysqli_connect_error());
+    }
 ?>
